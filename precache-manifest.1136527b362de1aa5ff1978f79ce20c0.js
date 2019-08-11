23a60d593c6568fecac1d3edaebacaba7e9042f4
cristopher yusuke <cristopher.yusuke@gmail.com>
2019-08-11 22:34:19 +0100

    Updates

---
+++ precache-manifest.1136527b362de1aa5ff1978f79ce20c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d504e722f579dbb8c432507deca565e6",
+    "url": "/tacaro/index.html"
+  },
+  {
+    "revision": "f8d16fbceefa6f53c992",
+    "url": "/tacaro/static/css/main.c8b3cf83.chunk.css"
+  },
+  {
+    "revision": "96cf8b153ab33e0f929e",
+    "url": "/tacaro/static/js/2.4178f64d.chunk.js"
+  },
+  {
+    "revision": "f8d16fbceefa6f53c992",
+    "url": "/tacaro/static/js/main.4a381299.chunk.js"
+  },
+  {
+    "revision": "6f09023729f9ab401186",
+    "url": "/tacaro/static/js/runtime~main.6057aab3.js"
+  }
+]);
