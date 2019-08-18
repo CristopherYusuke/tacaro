133c9ed71c255211ca8d774262872eaef0af3200
cristopher yusuke <cristopher.yusuke@gmail.com>
2019-08-19 00:34:22 +0100

    change some files and side-menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
 import React, { useState } from 'react';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import './App.sass';
-
+import SideMenu from './component/side-menu';
 
 function App() {
   const [hideMenu, setHideMenu] = useState(true);
 
   return (
     <>
-      <div className="hero is-info">
-        <div className="hero-body">
-          <button type="button" className="button" onClick={() => setHideMenu(!hideMenu)}>
-            <i className="fas fa-bars fa-lg" />
-          </button>
-          <div className="container">
-            <h1 className="title">
-            TaCaro App
-            </h1>
-            <h2 className="subtitle">
-            application to see how expensive is based on salary and another stuffs
-            </h2>
-          </div>
-        </div>
-      </div>
+      <header>
+        <button type="button" className="button" onClick={() => setHideMenu(!hideMenu)}>
+          <i className="fas fa-bars fa-lg" />
+        </button>
+        ta caro
+      </header>
       { hideMenu && (
-        <aside className="menu">
-          <p className="menu-label">
-          nada a teste
-          </p>
-          <ul className="menu-list">
-            <li><a>Dashboard</a></li>
-            <li><a>Customers</a></li>
-          </ul>
-          <p className="menu-label">
-          teste
-          </p>
-        </aside>
+        <SideMenu />
       )}
-
     </>
   );
 }
---
+++ src/component/side-menu/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './side-menu.sass';
+
+export default function SideMenu() {
+  return (
+    <aside className="menu">
+      <p className="menu-label">
+          nada a teste
+      </p>
+      <ul className="menu-list">
+        <li> asdf </li>
+      </ul>
+      <p className="menu-label">
+          teste
+      </p>
+    </aside>
+  );
+}
